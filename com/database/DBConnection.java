5adc8e0c064cb21b5a5f726110bba9b6808b1ded
Jyberion <codelyoko197@gmail.com>
2023-10-15 04:29:42 -0400

    update

---
+++ com/database/DBConnection.java
@@ -0,0 +1,30 @@
+package com.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+  private Connection connection;
+
+  public DBConnection() {
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
+      this.connection = DriverManager.getConnection(
+        "jdbc:mysql://localhost:3306/mmorpg", 
+        "username", 
+        "password"
+      );
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+  
+  public Connection getConnection() {
+    return this.connection; 
+  }
+
+}
